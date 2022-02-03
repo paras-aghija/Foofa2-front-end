61e8ff1987f5016a3fc0675c140d049ec5aa26f6
harshit singhal <supernovahs@pop-os.localdomain>
2022-02-03 10:09:10 +0530

    send

--- packages/react-app/src/views/Home.jsx
+++ packages/react-app/src/views/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useContractReader } from "eth-hooks";
 import { ethers } from "ethers";
+import {useState} from "react";
 
 /**
  * web3 props can be passed from '../App.jsx' into your local view component for use
@@ -9,11 +10,11 @@ import { ethers } from "ethers";
  * @param {*} readContracts contracts from current chain already pre-loaded using ethers contract module. More here https://docs.ethers.io/v5/api/contract/contract/
  * @returns react component
  */
-function Home({ yourLocalBalance, readContracts }) {
+function Home({ yourLocalBalance, readContracts,address }) {
   // you can also use hooks locally in your component of choice
   // in this case, let's keep track of 'purpose' variable from our contract
   const purpose = useContractReader(readContracts, "YourContract", "purpose");
-
+  const url = `https://api.covalenthq.com/v1/42/address/${address}/balances_v2/?quote-currency=USD&format=JSON&nft=true&no-nft-fetch=false&key=ckey_1d7288b1bd29481ba9c8415d038`
   return (
     <div>
       <div style={{ margin: 32 }}>
